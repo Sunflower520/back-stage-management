739df383e3ba581b1f7647c3b17c39ac115036da
刘温暖 <973421949@qq.com>
2021-04-08 23:35:22 +0800

    修改-登录页面，完善配置文件

---
+++ src/views/Login/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+    <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
+      <el-tab-pane label="登录" name="first"><SignIn /></el-tab-pane>
+      <el-tab-pane label="注册" name="second"><register /></el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+<script>
+import SignIn from "./SignIn";
+import register from "./register";
+export default {
+  components: {
+    SignIn,
+    register,
+  },
+  data() {
+    return {
+      activeName: "first",
+    };
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.home {
+  margin-top: 10vh;
+  /deep/ .el-tabs__header {
+    border-bottom: none;
+    .el-tabs__nav {
+      margin-left: 50%;
+      transform: translate(-50%, 0) !important;
+      border-bottom: none;
+      .el-tabs__item {
+        font-size: 20px;
+      }
+    }
+  }
+  /deep/ .el-tabs__content {
+    text-align: center;
+  }
+}
+</style>
---
+++ src/views/Login/register.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="register">2222</div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="scss" scoped>
+.register {
+
+}
+</style>
