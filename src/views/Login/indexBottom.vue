b2a025ac95845e564d5147a82482249bff42be31
刘温暖 <973421949@qq.com>
2021-06-29 17:25:25 +0800

    底部图表完成

--- src/views/Login/indexBottom.vue
+++ src/views/Login/indexBottom.vue
@@ -1,1 +1,302 @@
+<template>
+  <div class="indexButton">
+    <div class="indexButton_left">
+      <el-table :data="tableData" border style="width: 100%" height="230">
+        <el-table-column prop="date" label="灌区名称" width="100">
+        </el-table-column>
+        <el-table-column prop="name" label="水位" width="50"> </el-table-column>
+        <el-table-column prop="address" label="开度" width="50">
+        </el-table-column>
+        <el-table-column prop="aaa" label="检测时间"> </el-table-column>
+      </el-table>
+    </div>
+    <div class="indexButton_between">
+      <div id="waterStatisticsMain" style="width: 100%; height: 100%"></div>
+    </div>
+    <div class="indexButton_right"></div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          date: '双合灌区',
+          name: '11',
+          address: '15',
+          aaa: '06-09 18:21:03',
+        },
+        {
+          date: '木系噶灌区',
+          name: '9',
+          address: '5',
+          aaa: '06-09 18:31:03',
+        },
+        {
+          date: '东升灌区',
+          name: '7',
+          address: '11',
+          aaa: '06-09 18:20:03',
+        },
+        {
+          date: '查干沐沦灌区',
+          name: '8',
+          address: '7',
+          aaa: '06-09 18:23:03',
+        },
+        {
+          date: '钓鱼台灌区',
+          name: '10',
+          address: '12',
+          aaa: '06-09 18:24:03',
+        },
+      ],
+    }
+  },
+  mounted() {
+    let myChart = this.$echarts.init(
+      document.getElementById('waterStatisticsMain')
+    )
+    myChart.setOption({
+      title: {
+        text: '灌区水量统计',
+        textStyle: {
+          //图例文字的样式
+          color: '#fff',
+        },
+        // subtext: 'Feature Sample: Gradient Color, Shadow, Click Zoom'
+      },
+      grid: {
+        x: 50,
+        y: 45,
+        x2: 50,
+        y2: 20,
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross',
+          crossStyle: {
+            color: '#999',
+          },
+        },
+      },
 
+      legend: {
+        right: '5%',
+        orient: 'horizontal',
+        data: ['蒸发量', '降水量', '平均温度'],
+        textStyle: {
+          //图例文字的样式
+          color: '#fff',
+          fontSize: 12,
+        },
+      },
+      xAxis: [
+        {
+          type: 'category',
+          data: [
+            '1月',
+            '2月',
+            '3月',
+            '4月',
+            '5月',
+            '6月',
+            '7月',
+            '8月',
+            '9月',
+            '10月',
+            '11月',
+            '12月',
+          ],
+          axisPointer: {
+            type: 'shadow',
+          },
+          axisLabel: {
+            textStyle: {
+              color: '#fff',
+            },
+          },
+          splitLine: {
+            show: true,
+            lineStyle: {
+              color: 'skyblue',
+              //   type: 'dashed',
+            },
+          },
+        },
+      ],
+      yAxis: [
+        {
+          type: 'value',
+          //   name: '水量',
+          min: 0,
+          max: 250,
+          interval: 50,
+          axisLabel: {
+            formatter: '{value} ml',
+            textStyle: {
+              color: '#fff',
+            },
+          },
+        },
+        {
+          type: 'value',
+          //   name: '温度',
+          min: 0,
+          max: 25,
+          interval: 5,
+          axisLabel: {
+            formatter: '{value} °C',
+            textStyle: {
+              color: '#fff',
+            },
+          },
+          splitLine: {
+            show: true,
+            lineStyle: {
+              color: 'skyblue',
+              //   type: 'dashed',
+            },
+          },
+        },
+      ],
+      series: [
+        {
+          name: '蒸发量',
+          type: 'bar',
+          data: [
+            2.0,
+            4.9,
+            7.0,
+            23.2,
+            25.6,
+            76.7,
+            135.6,
+            162.2,
+            32.6,
+            20.0,
+            6.4,
+            3.3,
+          ],
+        },
+        {
+          name: '降水量',
+          type: 'bar',
+          data: [
+            2.6,
+            5.9,
+            9.0,
+            26.4,
+            28.7,
+            70.7,
+            175.6,
+            182.2,
+            48.7,
+            18.8,
+            6.0,
+            2.3,
+          ],
+        },
+        {
+          name: '平均温度',
+          type: 'line',
+          yAxisIndex: 1,
+          data: [
+            2.0,
+            2.2,
+            3.3,
+            4.5,
+            6.3,
+            10.2,
+            20.3,
+            23.4,
+            23.0,
+            16.5,
+            12.0,
+            6.2,
+          ],
+        },
+      ],
+    })
+  },
+  methods: {},
+}
+</script>
+<style lang="scss" scoped>
+.indexButton {
+  box-sizing: border-box;
+  width: 100%;
+  height: 27%;
+  margin-top: 0.5%;
+  .indexButton_left {
+    box-sizing: border-box;
+    float: left;
+    width: 20%;
+    height: 100%;
+    padding: 10px 15px;
+    border: 2px solid skyblue;
+    border-radius: 16px;
+    //   表头字体颜色
+    /deep/ .el-table thead {
+      color: skyblue;
+    }
+    //   表身字体颜色
+    /deep/ .el-table {
+      color: #fff;
+    }
+    //   表头背景颜色
+    /deep/ .el-table th,
+    .el-table tr {
+      background-color: rgb(5, 14, 59) !important;
+    }
+    //   表身背景颜色
+    /deep/.el-table__row {
+      background-color: rgb(5, 14, 59) !important;
+    }
+    //   选中颜色透明
+    /deep/ .el-table--enable-row-hover .el-table__body tr:hover > td {
+      background-color: transparent;
+    }
+    //   边框线颜色开始
+    //   /deep/ .el-table th.is-leaf {
+    //     border-bottom: 1px solid skyblue !important;
+    //   }
+    //   /deep/ .el-table td,
+    //   .el-table th.is-leaf {
+    //     border-bottom: 1px solid skyblue !important;
+    //   }
+    //   /deep/ .el-table--border td,
+    //   .el-table--border th,
+    //   .el-table__body-wrapper
+    //     .el-table--border.is-scrolling-left
+    //     ~ .el-table__fixed {
+    //     border-right: 1px solid skyblue !important;
+    //   }
+    //   /deep/ .el-table--border, .el-table--group {
+    //       border: 1px solid skyblue !important;
+    //   }
+    // 边框线颜色结束
+  }
+  .indexButton_between {
+    box-sizing: border-box;
+    float: left;
+    width: 59%;
+    height: 100%;
+    padding: 10px 15px;
+    margin: 0 0.5%;
+    border: 2px solid skyblue;
+    border-radius: 16px;
+  }
+  .indexButton_right {
+    box-sizing: border-box;
+    float: right;
+    width: 20%;
+    height: 100%;
+    padding: 10px 15px;
+    border: 2px solid skyblue;
+    border-radius: 16px;
+  }
+}
+</style>
--- src/views/Login/indexMiddle.vue
+++ src/views/Login/indexMiddle.vue
@@ -3,12 +3,41 @@
     <!-- 左侧 -->
     <div class="indexMiddle_left">
       <!-- 代办事项 -->
-      <div class="Agent"></div>
-      <!-- 玫瑰图表 -->
-      <div class="roseChart"></div>
+      <div class="Agent">
+        <div class="title">
+          <p>代办事项</p>
+        </div>
+        <div class="expect">
+          <p>
+            <span class="completed">
+              <b>20</b>
+            </span>
+            <i style="color: green">已完成</i>
+          </p>
+          <p>
+            <span class="incomplete"><b>20</b></span>
+            <i style="color: red">未完成</i>
+          </p>
+          <p>
+            <span class="overdue"><b>20</b></span>
+            <i style="color: orange">逾期</i>
+          </p>
+        </div>
+      </div>
+      <!-- 图表 -->
+      <div class="chart">
+        <!-- 玫瑰图表 -->
+        <div class="roseChart" style="width: 50%" height="100%">
+          <div id="roseChartMain" style="width: 100%; height: 100%"></div>
+        </div>
+        <!-- 环形图表 -->
+        <div class="annularChart" style="width: 50%" height="100%">
+          <div id="annularChartMain" style="width: 100%; height: 100%"></div>
+        </div>
+      </div>
       <!-- 灌区概况 -->
       <div class="irrigatedSurvey">
-        <el-table :data="tableData" border style="width: 100%" height="260">
+        <el-table :data="tableData" border style="width: 100%" height="270">
           <el-table-column prop="date" label="灌区名称"> </el-table-column>
           <el-table-column prop="name" label="所属位置"> </el-table-column>
           <el-table-column prop="address" label="所属河流"> </el-table-column>
@@ -19,7 +48,14 @@
     <!-- 中间 -->
     <div class="indexMiddle_between"></div>
     <!-- 右侧 -->
-    <div class="indexMiddle_right"></div>
+    <div class="indexMiddle_right">
+      <!-- 降雨气象信息 -->
+      <div class="meteorological"></div>
+      <!-- 近24小时的雨量 -->
+      <div class="rainfall"></div>
+      <!-- 雷达图 -->
+      <div class="radarMap"></div>
+    </div>
   </div>
 </template>
 <script>
@@ -60,7 +96,133 @@ export default {
       ],
     }
   },
-  methods: {},
+  mounted() {
+    this.roseChartMain() // 玫瑰图表
+    this.annularChartMain() // 环形图表
+  },
+  methods: {
+    // 玫瑰图表
+    roseChartMain() {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = this.$echarts.init(document.getElementById('roseChartMain'))
+      // 绘制图表
+      myChart.setOption({
+        legend: {
+          // orient: 'vertical',
+          bottom: 'bottom',
+          itemGap: 5,
+          textStyle: {
+            //图例文字的样式
+            color: '#fff',
+            fontSize: 10,
+          },
+        },
+        tooltip: {
+          trigger: 'item',
+        },
+        series: [
+          {
+            name: '面积模式',
+            type: 'pie',
+            radius: '50%',
+            center: ['50%', '30%'],
+            roseType: 'area',
+            label: {
+              fontSize: 12,
+              color: '#fff',
+            },
+            itemStyle: {
+              normal: {
+                label: {
+                  show: false,
+                  textStyle: {
+                    //图例文字的样式
+                    color: '#fff',
+                    fontSize: 10,
+                  },
+                },
+                labelLine: {
+                  show: false,
+                  // length: 1,
+                },
+              },
+            },
+            data: [
+              { value: 40, name: 'rose1' },
+              { value: 38, name: 'rose2' },
+              { value: 32, name: 'rose3' },
+              { value: 30, name: 'rose4' },
+              { value: 28, name: 'rose5' },
+            ],
+          },
+        ],
+      })
+    },
+    annularChartMain() {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = this.$echarts.init(
+        document.getElementById('annularChartMain')
+      )
+      // 绘制图表
+      myChart.setOption({
+        tooltip: {
+          trigger: 'item',
+        },
+        // legend: {
+        //   // orient: 'vertical',
+        //   right: 'right',
+        //   top: '5%',
+        //   itemGap: 5,
+        //     textStyle: {
+        //     //图例文字的样式
+        //     color: '#fff',
+        //     fontSize: 10,
+        //   },
+        // },
+        legend: {
+          // orient: 'vertical',
+          bottom: 'bottom',
+          itemGap: 5,
+          textStyle: {
+            //图例文字的样式
+            color: '#fff',
+            fontSize: 10,
+          },
+        },
+        series: [
+          {
+            name: '面积模式',
+            type: 'pie',
+            radius: ['30%', '50%'],
+            center: ['50%', '30%'],
+            avoidLabelOverlap: false,
+            label: {
+              show: false,
+              position: 'center',
+            },
+            emphasis: {
+              label: {
+                show: true,
+                fontSize: '10',
+                fontWeight: 'bold',
+                color: '#fff',
+              },
+            },
+            labelLine: {
+              show: false,
+            },
+            data: [
+              { value: 1048, name: 'annular1' },
+              { value: 735, name: 'annular2' },
+              { value: 580, name: 'annular3' },
+              { value: 484, name: 'annular4' },
+              { value: 300, name: 'annular5' },
+            ],
+          },
+        ],
+      })
+    },
+  },
 }
 </script>
 <style lang="scss" scoped>
@@ -84,9 +246,77 @@ export default {
       margin-bottom: 2%;
       border: 2px solid skyblue;
       border-radius: 16px;
+      .title {
+        width: 30%;
+        height: 100%;
+        float: left;
+        p {
+          color: #fff;
+          font-size: 20px;
+          font-weight: 600;
+        }
+      }
+      .expect {
+        display: flex;
+        width: 70%;
+        height: 100%;
+        float: right;
+        p {
+          flex: 1;
+          padding: 10px;
+          .completed {
+            box-sizing: border-box;
+            display: block;
+            height: 60%;
+            border: 6px solid green;
+            border-radius: 50% 50%;
+            b {
+              color: #fff;
+              margin-left: 20%;
+              font-size: 22px;
+              line-height: 36px;
+            }
+          }
+          .incomplete {
+            box-sizing: border-box;
+            display: block;
+            height: 60%;
+            border: 6px solid red;
+            border-radius: 50% 50%;
+            b {
+              color: #fff;
+              margin-left: 20%;
+              font-size: 22px;
+              line-height: 36px;
+            }
+          }
+          .overdue {
+            box-sizing: border-box;
+            display: block;
+            height: 60%;
+            border: 6px solid orange;
+            border-radius: 50% 50%;
+            b {
+              color: #fff;
+              margin-left: 20%;
+              font-size: 22px;
+              line-height: 36px;
+            }
+          }
+          i {
+            box-sizing: border-box;
+            display: block;
+            height: 40%;
+            text-align: center;
+            line-height: 44px;
+            font-weight: 600;
+          }
+        }
+      }
     }
     // 左侧-玫瑰图表
-    .roseChart {
+    .chart {
+      display: flex;
       box-sizing: border-box;
       width: 100%;
       height: 30%;
@@ -112,7 +342,7 @@ export default {
       //   }
       //   表头字体颜色
       /deep/ .el-table thead {
-        color: #fff;
+        color: skyblue;
       }
       //   表身字体颜色
       /deep/ .el-table {
@@ -131,25 +361,25 @@ export default {
       /deep/ .el-table--enable-row-hover .el-table__body tr:hover > td {
         background-color: transparent;
       }
-    //   边框线颜色开始
-    //   /deep/ .el-table th.is-leaf {
-    //     border-bottom: 1px solid skyblue !important;
-    //   }
-    //   /deep/ .el-table td,
-    //   .el-table th.is-leaf {
-    //     border-bottom: 1px solid skyblue !important;
-    //   }
-    //   /deep/ .el-table--border td,
-    //   .el-table--border th,
-    //   .el-table__body-wrapper
-    //     .el-table--border.is-scrolling-left
-    //     ~ .el-table__fixed {
-    //     border-right: 1px solid skyblue !important;
-    //   }
-    //   /deep/ .el-table--border, .el-table--group {
-    //       border: 1px solid skyblue !important;
-    //   }
-    // 边框线颜色结束
+      //   边框线颜色开始
+      //   /deep/ .el-table th.is-leaf {
+      //     border-bottom: 1px solid skyblue !important;
+      //   }
+      //   /deep/ .el-table td,
+      //   .el-table th.is-leaf {
+      //     border-bottom: 1px solid skyblue !important;
+      //   }
+      //   /deep/ .el-table--border td,
+      //   .el-table--border th,
+      //   .el-table__body-wrapper
+      //     .el-table--border.is-scrolling-left
+      //     ~ .el-table__fixed {
+      //     border-right: 1px solid skyblue !important;
+      //   }
+      //   /deep/ .el-table--border, .el-table--group {
+      //       border: 1px solid skyblue !important;
+      //   }
+      // 边框线颜色结束
     }
   }
   //   中间
@@ -165,6 +395,33 @@ export default {
     float: right;
     width: 20%;
     height: 100%;
+    .meteorological {
+      box-sizing: border-box;
+      width: 100%;
+      height: 20%;
+      padding: 10px 15px;
+      margin-bottom: 2%;
+      border: 2px solid skyblue;
+      border-radius: 16px;
+    }
+    .rainfall {
+      box-sizing: border-box;
+      width: 100%;
+      height: 30%;
+      padding: 10px 15px;
+      margin-bottom: 2%;
+      border: 2px solid skyblue;
+      border-radius: 16px;
+    }
+    .radarMap {
+      box-sizing: border-box;
+      width: 100%;
+      height: 47%;
+      padding: 10px 15px;
+      //   margin-bottom: 2%;x
+      border: 2px solid skyblue;
+      border-radius: 16px;
+    }
   }
 }
 </style>
